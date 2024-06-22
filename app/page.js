0f9568630deef3d911475af57b4e37ac1f350c29
Mohamed amaan <mohamedamaan319@gmail.com>
2024-06-22 13:37:03 +0530

    frontend updated

--- app/page.js
+++ app/page.js
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button"
 
 const page = () => {
   return (
-    <div>
-      <h1 className='red' >HELO Iam mohamed amaan</h1>
-      <Button>Amaan</Button>
+    <div className='p-10 flex flex-col items-center justify-center' >
+      <h1 className='red font-bold text-[22px]'>Welcome to AI Mock Interview</h1>
+      <a href={"/dashboard"} ><h1><Button>Start</Button></h1></a>
     </div>
   )
 }
