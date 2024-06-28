2453e89bbde09c9685d9fbfd95c72b707bd78e83
Mohamed amaan <mohamedamaan319@gmail.com>
2024-06-28 09:35:54 +0530

    loding circle added

--- app/_components/Contect.jsx
+++ app/_components/Contect.jsx
@@ -1,32 +1,48 @@
 "use client";
 import { db } from "@/utils/db";
 import { Newsletter } from "@/utils/schema";
+import { LoaderCircle } from "lucide-react";
 import moment from "moment";
 import React, { useState } from "react";
 import { toast } from "sonner";
 
 const Contect = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
     console.log(name, email, message);
+    if (name && email && message) {
+      setLoading(true);
+      try {
+        const resp = await db.insert(Newsletter).values({
+          newName: name,
+          newEmail: email,
+          newMessage: message,
+          createdAt: moment().format("YYYY-MM-DD"),
+        });
 
-    const resp = await db.insert(Newsletter).values({
-      newName: name,
-      newEmail: email,
-      newMessage: message,
-      createdAt: moment().format("YYYY-MM-DD"),
-    });
-
-    if (resp) {
-      toast("User Response recorded successfully");
-      setName('')
-      setEmail('')
-      setMessage('')
+        if (resp) {
+          toast("User Response recorded successfully");
+          setName("");
+          setEmail("");
+          setMessage("");
+        } else {
+          toast("Error recording response");
+        }
+      } catch (error) {
+        console.log(error);
+        toast("Error recording response");
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      toast("No data entered");
     }
   };
   return (
@@ -63,7 +79,11 @@ const Contect = () => {
             type="submit"
             className="px-6 py-3 text-lg font-semibold bg-black text-white rounded-lg shadow-lg hover:bg-gray-700"
           >
-            Send Message
+            {loading ? (
+              <LoaderCircle className="animate-spin" />
+            ) : (
+              "Send Message"
+            )}
           </button>
         </form>
       </div>
