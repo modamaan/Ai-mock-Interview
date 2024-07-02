e2f4f3a417d6c295d5e9eb78a59fa222d5e621a4
Mohamed amaan <mohamedamaan319@gmail.com>
2024-07-03 00:57:02 +0530

    Default theme change to white

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
           <Toaster />
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="white"
             enableSystem
             disableTransitionOnChange
           >
